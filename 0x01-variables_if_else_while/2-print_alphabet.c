30f5553b46a66736408146546b078d23630ff485
Mawejje Mark William <mawejjemarkwilliam@gmail.com>
2023-09-09 10:17:31 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - This is the main program function
+ * Return: returns 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
