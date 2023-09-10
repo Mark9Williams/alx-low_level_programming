d16899c7c49903b4f0edf10064edb33f5b647d3e
Mawejje Mark William <mawejjemarkwilliam@gmail.com>
2023-09-10 00:03:02 -0700

    5-print_numbers files

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This is where the program starts
+ * Return: returns 0
+ */
+/* Program that prints all single digit numbers of base 10 */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	return (0);
+}
