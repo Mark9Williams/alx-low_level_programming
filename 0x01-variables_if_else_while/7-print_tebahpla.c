e5f7724005856b1b93e3268f727f6c5cc6571871
Mawejje Mark William <mawejjemarkwilliam@gmail.com>
2023-09-10 01:37:55 -0700

    7-print_tebahpla files

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - This is the main program fuction
+ * Return: rturns 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
