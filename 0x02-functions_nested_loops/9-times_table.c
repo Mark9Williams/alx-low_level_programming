569b26bd6a487a02cacbcba7f5c4a8e83c38daa2
Mawejje Mark William <mawejjemarkwilliam@gmail.com>
2023-09-14 02:57:03 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ * Return: returns nothing
+ */
+
+void times_table(void)
+{
+	int row, column, n;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			n = row * column;
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
+				if (column < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+					_putchar('$');
+			}
+			else
+			{
+				_putchar(n + '0');
+				if (column < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+					_putchar('$');
+			}
+		}
+		_putchar('\n');
+	}
+}
