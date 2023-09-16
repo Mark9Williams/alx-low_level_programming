4d0cc9bcc363bf7a86b6a896bab433117767890c
Mawejje Mark William <mawejjemarkwilliam@gmail.com>
2023-09-16 03:10:30 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - This is the main function of the program
  * Return: Always returns 0
@@ -7,18 +6,32 @@
 
 int main(void)
 {
-	unsigned long int n1 = 1, n2 = 2, n3, n;
+	unsigned long int i, bef = 1, aft = 2, l = 1000000000;
+	unsigned long int bef1, bef2, aft1, aft2;
 
-	printf("%lu, %lu", n1, n2);
+	printf("%lu", bef);
 
-	for (n = 3; n <= 98; n++)
+	for (i = 1; i < 91; i++)
 	{
-		n3 = n1 + n2;
-		if (n3 < n1 || n3 < n2)
-			break;
-		printf(", %lu", n3);
-		n1 = n2;
-		n2 = n3;
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
+	}
+
+	bef1 = (bef / l);
+	bef2 = (bef % l);
+	aft1 = (aft / l);
+	aft2 = (aft % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", aft1 + (aft2 / l));
+		printf("%lu", aft2 % l);
+
+		aft1 = aft1 + bef1;
+		bef1 = aft1 - bef1;
+		aft2 = aft2 + bef2;
+		bef2 = aft2 - bef2;
 	}
 	printf("\n");
 	return (0);
