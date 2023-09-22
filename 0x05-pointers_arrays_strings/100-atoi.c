1125a32fd754d3a8e5f266968d3021149c0107b3
Mawejje Mark William <mawejjemarkwilliam@gmail.com>
2023-09-22 10:17:09 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _toi - converts a string to an integer
+ * @s: points to the first character of the string
+ * Return: return an integer in the string
+ */
+int _atoi(char *s)
+{
+	double result, sign = 1;
+
+	for (; *s; s++)
+	{
+		if (*s == '-')
+		{
+			sign = -sign;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			while (*s >= '0' && *s <= '9')
+			{
+				result = result * 10 + (*s - '0');
+				s++;
+			}
+			break;
+		}
+	}
+	return (result * sign);
+}
