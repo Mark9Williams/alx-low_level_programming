e23d17f8788cc71e1eae81ec70f28c2e8622a97f
Mawejje Mark William <mawejjemarkwilliam@gmail.com>
2023-09-23 08:26:14 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - changes all case letters of a string to uppercase
+ * @str: points the string
+ * Return: returns a pointer to the string
+ */
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 'a' + 'A';
+		}
+		i++;
+	}
+	return (str);
+}
