9cda069b4f469eb87880d49686f38933dafd330f
Mawejje Mark William <mawejjemarkwilliam@gmail.com>
2023-10-22 12:10:27 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul - a function that multiplies two integers
+ * @a: placeholder of one integer
+ * @b: placeholder of another integer
+ * Return: returns the result of multiplication
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
