2a1c64d17966ae4e950727ddd4e201073c206b9a
Mawejje Mark William <mawejjemarkwilliam@gmail.com>
2023-10-22 11:18:15 -0700

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+/**
+ * _isupper - checks for upper case character.
+ * @c: placeholder for the character.
+ * Return: returns an integer.
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0x04. C - More functions, more nested loops: Understanding the nitigrities of C functions.
