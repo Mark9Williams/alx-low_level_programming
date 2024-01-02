5054a5d5cea9649a82e04cac96c97883563fa46b
Mawejje Mark William <mawejjemarkwilliam@gmail.com>
2024-01-01 20:41:34 -0800

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r:the buffer that will store the result
+ * @size_r: is the buffer size
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	
