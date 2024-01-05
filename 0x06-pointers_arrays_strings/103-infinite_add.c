02c1ab3fa7a83b55402118aeb9c11e26a7ba4b73
Mawejje Mark William <mawejjemarkwilliam@gmail.com>
2024-01-04 22:28:03 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 /**
  * infinite_add - adds two numbers
  * @n1: first number
@@ -10,17 +10,17 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int carry = 0, index_n1 = strlen(n1) - 1, index_res = 0;
-	int index_n2 = strlen(n2) - 1, index_result = 0;
+	int carry = 0, index_n1 = strlen(n1) - 1, index_res = 0, sum;
+	int index_n2 = strlen(n2) - 1, i, j, digit_sum, digit_n1, digit_n2;
 
 	while (index_n1 >= 0 || index_n2 >= 0)
 	{
-		int digit_n1 = (index_n1 >= 0) ? (n1[index_n1] - '0') : 0;
-		int digit_n2 = (index_n2 >= 0) ? (n2[index_n2] - '0') : 0;
-		int sum = digit_n1 + digit_n2 + carry;
+		digit_n1 = (index_n1 >= 0) ? (n1[index_n1] - '0') : 0;
+		digit_n2 = (index_n2 >= 0) ? (n2[index_n2] - '0') : 0;
+		sum = digit_n1 + digit_n2 + carry;
 
 		carry = sum / 10;
-		int digit_sum = sum % 10;
+		digit_sum = sum % 10;
 
 		if (index_res < size_r - 1)
 		{
@@ -46,7 +46,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			}
 	}
 	r[index_res] = 0;
-	for (int i = 0, j = index_res - 1; i < j; i++, j--)
+	for (i = 0, j = index_res - 1; i < j; i++, j--)
 	{
 	int temp = r[i];
 
