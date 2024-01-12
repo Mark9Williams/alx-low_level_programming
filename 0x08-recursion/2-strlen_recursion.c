c9148b957b9ab6a928f1e0925f259511bbab747f
Mark9Williams <markwilliam023@DESKTOP-K1RM50H>
2024-01-12 03:25:45 +0300

    length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _strlen_recursion - returns the length of the string
+  * @s: placehoder for a pointer to a string
+  * Return: returns the length of the string
+  */
+int _strlen_recursion(char *s)
+{
+	if (*str == '\0')
+	{
+		int n = 0;
+
+		return;
+	}
+	_strlen_recursion(s + 1);
+	n++;
+	return (n);
+}
