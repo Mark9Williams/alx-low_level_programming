6df1a07e60a6d98a428f43620bb8121f8a340fe3
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-01-22 11:40:52 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - stores record of a dog
+  * @name: name of a dog
+  * @age: age of a dog
+  * @owner: owner of the dog
+  * Description: It stores the records of a dog in a single variable
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
